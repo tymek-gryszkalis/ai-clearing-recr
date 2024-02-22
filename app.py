9e50d7fdf898c99a0a43a4088a872a250c1081df
Sudowiec <lalarsek@gmail.com>
2024-02-22 16:05:24 +0100

    Car database set up

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    make = db.Column(db.String(30), nullable = False)
+    model = db.Column(db.String(30), nullable = False)
+    rate = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"{self.make} {self.model}"
 
 @app.route("/")
 def index():
