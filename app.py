73e5aabb01e9bdf8c79cc91d972534e766bf89a8
Sudowiec <lalarsek@gmail.com>
2024-02-23 17:57:47 +0100

    fix

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def popular():
             raise Exception
     except:
         abort(400)
-    sqlQuery = text(f"SELECT 'rate.carid', COUNT(*) FROM rate GROUP BY 'rate.carid' LIMIT {amount}")
+    sqlQuery = text(f"SELECT 'rate.carid', COUNT(*) FROM rate GROUP BY rate.carid LIMIT {amount}")
     r = db.session.execute(sqlQuery).fetchall()
     output = []
     for i in r:
