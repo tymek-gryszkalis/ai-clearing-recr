c7ccc731e920b6a7ded595e47afb75210162c602
Sudowiec <lalarsek@gmail.com>
2024-02-23 08:47:59 +0100

    Removed obsolete tests

--- tests.py
+++ tests.py
@@ -4,19 +4,6 @@ import requests
 
 URL = "http://127.0.0.1:5000/"
 
-class APIStructureTests(TestCase):
-    def testStatusOfCode200(self):
-        result = requests.get(f"{URL}cars")
-        assert result.status_code == 200
-
-    def testStatusOfCode400(self):
-        result = requests.get(f"{URL}popular", json = {"invalidKey" : "invalidValue"})
-        assert result.status_code == 400
-
-    def testStatusOfCode404(self):
-        result = requests.get(f"{URL}doesNotExist")
-        assert result.status_code == 404
-
 class postCarsTests(TestCase):
     def testPostCarValidBody(self):
         result = requests.post(f"{URL}cars", json = {"make" : "honda", "model" : "civic"})
